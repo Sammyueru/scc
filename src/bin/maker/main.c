edab5e19dba31d1b810b0e0a8fceded6e1d09bfd
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-03-16 13:23:19 -0600

    worked on maker

--- src/bin/maker/main.c
+++ src/bin/maker/main.c
@@ -1,7 +1,19 @@
 /* SCC: Maker - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[]) {
+    char* command;
+    for (int i = 0; i < argc; i++) {
+        switch (argv[i][0]) {
+        case '-': {
+        } break;
+        default: {
+            command = (char*)malloc(sizeof(argv[i]));
+            command = *argv[i];
+        } break;
+        }
+    }
     return 0;
 }
 
