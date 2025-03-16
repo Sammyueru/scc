89e9a5174fc0dd75ba18df072de63921daa98f41
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-03-16 11:41:18 -0600

    added hqas

---
+++ src/bin/hqas/main.cpp
@@ -0,0 +1,8 @@
+/* SCC: Hardware Quantum Assembler - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#include <iostream>
+#include <libqasm/v3x/cqasm-python.hpp>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
