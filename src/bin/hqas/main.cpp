0348d94ed752533a6aa7ac87896a0e96a3f9829b
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-03-17 07:57:18 -0600

    worked on hardware quantum assembler

--- src/bin/hqas/main.cpp
+++ src/bin/hqas/main.cpp
@@ -1,8 +1,30 @@
 /* SCC: Hardware Quantum Assembler - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
 #include <iostream>
+#include <cstring>
+#include <vector>
 #include <libqasm/v3x/cqasm-python.hpp>
 
 int main(int argc, char* argv[]) {
+    std::vector<std::string> input_files; // input hardware quantum assembly to be compiled
+    
+    for (int i = 0; i < argc; i++) {
+        size_t arglen = strlen(argv[i]); // the length of the argument currently being parsed
+        if (arglen < 1) break;
+
+        switch (argv[i][0]) {
+        case '-': { // the argument is a parameter
+            if (arglen < 2) {
+                std::cout << "{SCC: HQAS} ERROR: exiting due to no parameter in argument." << std::endl;
+                return -1;
+            }
+        } break;
+        default: { // the argument is an input file
+            input_files.push_back(std::string(argv[i]));
+        } break;
+        }
+    }
+main_end_argument_parser:
+
     return 0;
 }
 
