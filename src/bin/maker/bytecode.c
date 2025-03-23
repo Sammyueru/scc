b2e483f60f0e2fe847c455f687b23237c3e8ca30
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-03-23 09:01:17 -0600

    worked on maker bytecode

---
+++ src/bin/maker/bytecode.c
@@ -0,0 +1,8 @@
+/* SCC: Maker - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#include "bytecode.h"
+
+instruction** bc_compile() {
+    /* instruction** bc = (instruction**)malloc(); */
+    return malloc(0);
+}
+
--- src/bin/maker/bytecode.h
+++ src/bin/maker/bytecode.h
@@ -1,13 +1,19 @@
 /* SCC: Maker - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#ifndef SCC_MAKER_BYTECODE_H
+#define SCC_MAKER_BYTECODE_H
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
     typedef enum {
-        jump = 0,
+        nop = 0, /* no operation */
+        jmp = 1, /* jump to address in memory */
+        store = 2, /* store value in variable */
     } op;
     int16_t* in_types;
     int16_t out_type;
     void** inputs;
 } instruction;
+
+#endif
 
