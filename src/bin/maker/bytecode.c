8e31bb09b8898c66b8c407f5e563cc616e4f4876
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-04-19 06:44:36 -0600

    changed bytecode.c to use c89 comments

--- src/bin/maker/bytecode.c
+++ src/bin/maker/bytecode.c
@@ -3,7 +3,7 @@
 
 instruction** bc_compile(FILE* from) {
     instruction** bc = (instruction**)malloc(sizeof(instruction*) * 30);
-    // pre-initialize bytecode instructions
+    /* pre-initialize bytecode instructions */
     for (int i = 0; i < 30; i++) {
         bc[i] = (instruction*)malloc(sizeof(instruction));
     }
@@ -13,16 +13,16 @@ instruction** bc_compile(FILE* from) {
 
 void bc_free(instruction** bc) {
     for (int i = 0; i < (sizeof(bc)/sizeof(instruction*)); i++) {
-        free(bc[i]->in_types); // free inputs' types
+        free(bc[i]->in_types); /* free inputs' types */
 
-        // free inputs' elements
+        /* free inputs' elements */
         for (int ix = 0; ix < sizeof(bc[i]->inputs)/sizeof(void*); ix++) {
             free(bc[i]->inputs[ix]);
         }
-        free(bc[i]->inputs); // free inputs array
-        free (bc[i]); // free instruction element
+        free(bc[i]->inputs); /* free inputs array */
+        free (bc[i]); /* free instruction element */
     }
 
-    free(bc); // free instruction array
+    free(bc); /* free instruction array */
 }
 
