6b7f835bd389f87d22ba0a40afd0e1e43c2e21be
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-04-22 10:59:38 -0600

    added vector format to svg

---
+++ res/scripts/vf2svg/main.c
@@ -0,0 +1,8 @@
+/* SCC: res/scripts - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
+
