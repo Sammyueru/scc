1ee41315f3f9f3e00ae524d1c1c1b0a1443ecc85
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-04-26 18:09:34 -0600

    started working on classical shared sources

---
+++ src/bin/cshare/cpp/ast/astnodes.cpp
@@ -0,0 +1,4 @@
+/* SCC: Classical - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#include "astnodes.h"
+
+
---
+++ src/bin/cshare/cpp/ast/astnodes.h
@@ -0,0 +1,43 @@
+/* SCC: Classical - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
+#ifndef SCC_CSHARE_CPP_AST_H
+#define SCC_CSHARE_CPP_AST_H
+#include <iostream>
+#include <memory>
+
+class AST_Node {};
+
+class AST_Tree : AST_Node {
+public:
+    std::vector<std::shared_ptr<AST_Node>> contents;
+};
+
+class AST_Program : AST_Tree {};
+
+class AST_Variable : AST_Node {
+public:
+    std::string name;
+    std::string type;
+};
+
+class AST_Operation : AST_Node {
+public:
+    std::shared_ptr<AST_Node> left;
+    std::string op;
+    std::shared_ptr<AST_Node> right;
+};
+
+class AST_Asignment : AST_Node {
+public:
+    std::shared_ptr<AST_Variable> var;
+    std::string modifier;
+    std::shared_ptr<AST_Node> value;
+};
+
+class AST_If : AST_Tree {
+public:
+    AST_Operation condition;
+    std::vector<AST_If> else_ifs;
+    std::vector<std::shared_ptr<AST_Node>> else;
+};
+
+#endif
