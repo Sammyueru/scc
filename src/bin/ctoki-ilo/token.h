85b319d5972d8c2cc42b14f83b06dc9b3420dada
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-09-06 15:14:48 -0600

    update tokens

--- src/bin/ctoki-ilo/token.h
+++ src/bin/ctoki-ilo/token.h
@@ -12,37 +12,45 @@ public:
         Literal = 3,
         Macro = 4,
 
-        PERIOD = 6,
-        QUESTION = 7,
-        COLON = 8,
+        Punctuation = 6,
+        Colon = 8,
 
         jo = 12, /* variables */
         li = 13, /* assignment */
         ilo = 14, /* tools */
         lawa = 15, /* rules */
-        nanpa = 16, /* numbers */
+        nanpa = 16, /* numbers and reference counted pointers */
         ala = 17, /* not, 0 */
         mute = 18, /* specify a quantity of an array */
         lili = 19,
         nasin = 20, /* create functions */
         o = 21, /* call functions (in combination with nasin) */
         nimi = 22, /* name functions and variables */
-        ni = 23,
-        mi = 24,
-        sina = 25,
+        ni = 23, /* self or block creation with ".. ni:" */
+        mi = 24, /* program output like "toki mi '<string>' e sina" to print a string */
+        sina = 25, /* program input like "toki sina '<string>' e mi" to get user input string */
         kama = 26, /* events */
-        ijo = 27, /* objects */
+        ijo = 27, /* objects and dynamic/changing type */
         kute = 28,
         lape = 29,
-        ma = 30, /* namespaces? */
-        pini = 31, /* task completed */
-        pana = 32,
+        ma = 30, /* location labels and namespaces with "ma tomo <name>" */
+        pini = 31, /* task completed/end block */
+        pana = 32, /* return */
         pi = 33, /* access object or array elements */
         toki = 34, /* "toki sonpile tu { <C++ code> }" include C++ code, "toki sonpile { <C code> }" include C code */
-        weka = 35,
+        weka = 35, /* subtraction */
         wile = 36, /* require */
         wawa = 37,
         lipu = 38, /* records */
+        tawa = 39, /* go to label's location */
+        la = 40, /* conditionals */
+        anu = 41, /* conditional else */
+        lon = 42, /* memory access/pointers */
+        sin = 43, /* create new instance */
+        tenpo = 44, /* scheduling */
+        pali = 45, /* create variable with "<variable name> [li <variable type>] li pali e <value>" and create macros/rules */
+        e = 46,
+        en = 47, /* addition and 'and' */
 
         Comment = 255,
     };
