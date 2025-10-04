8d91fb1d0f492ec392e315ed0936d876ddf9c74e
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-10-04 09:51:15 -0600

    spc argument parser

--- src/bin/ct_spc/main.c
+++ src/bin/ct_spc/main.c
@@ -38,14 +38,48 @@ int main(int argc, char* argv[]) {
 
     /* parse arguments */
     for (int i = 0; i < argc; i++) {
-        
+        char arg = argv[i];
+        size_t argl /* current argument length */ = strnlen(arg, BOUND_16ui - 1);
+        if (argl < 1) continue;
+        switch (arg[0]) {
+        case '-': {
+            if (argl < 2) continue;
+            switch (arg[1]) {
+            case 'i': {
+                if (argc < i) break;
+                arg = argv[++i];
+                size_t argl = strnlen(arg, BOUND_16ui - 1);
+
+                size_t dir_idx = settings->inc_dirs_count++;
+                settings->inc_dirs[dir_idx] = (char*)malloc(argl + 1);
+                strncpy(settings->inc_dirs[dir_idx], arg, BOUND_16ui);
+            } break;
+            case 'o': {
+                if (argc < i) break;
+                arg = argv[++i];
+                size_t argl = strnlen(arg, BOUND_16ui - 1);
+
+                settings->output_dir = (char*)malloc(argl + 1);
+                strncpy(settings->output_dir, arg, BOUND_16ui);
+            } break;
+            case '-': {
+
+            } break;
+            default: break;
+            }
+        } break;
+        default: {
+            size_t dir_idx = settings->src_dirs_count++;
+            settings->src_dirs[dir_idx] = (char*)malloc(argl + 1);
+            strncpy(settings->src_dirs[dir_idx], arg, BOUND_16ui);
+        } break;
+        }
     }
 
     /* transpile */
     transpile(settings);
 
     /* cleanup */
-
     for (size_t i = 0; i < settings->src_dirs_count; i++) {
         free(settings->src_dirs[i]);
     }
