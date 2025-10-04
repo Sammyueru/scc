56a5bbcd09303fcae79971bb7eda4ea8b8c554cc
Samm <60794497+Sammyueru@users.noreply.github.com>
2025-10-04 13:42:34 -0600

    transpiler

--- src/bin/ct_spc/transpiler.c
+++ src/bin/ct_spc/transpiler.c
@@ -1,7 +1,9 @@
 /* SCC: Sam's Pre-C (SPC) - Copyright (c) 2025 Samm, See LICENSE.txt for license details */
 #include <stdio.h>
 #include <string.h>
+#include "transpiler.h"
 #include "defines.h"
+#include "utils/fs/patterns.h"
 
 char* convert_var_name(char* og_name) {
     if (!og_name) return NULL;
@@ -59,5 +61,22 @@ char* convert_var_name(char* og_name) {
 }
 
 uint16_t transpile(transpiler_settings* settings) {
+    char** source_files = NULL;
+    size_t source_files_count = 0;
+    for (size_t i = 0; i < settings->src_dirs_count; i++) {
+        fs_pattern_t fpattern = (fs_pattern_t)0;
+        size_t pinput_len = strnlen(settings->src_dirs[i], BOUND_16ui - 2) + 2;
+        char* pinput = (char*)malloc(pinput_len);
+        if (pinput == NULL) return 1;
+        memcpy(pinput, settings->src_dirs[i], pinput_len - 2);
+        pinput[pinput_len - 1] = '\0';
+        pinput[pinput_len - 2] = '*';
+
+        util_fs_pattern_match(&pattern, pinput, 0, NULL, NULL);
+        free(pinput);
+
+        util_fs_pattern_free(&pattern);
+    }
+    
     return 0; /* success */
 }
